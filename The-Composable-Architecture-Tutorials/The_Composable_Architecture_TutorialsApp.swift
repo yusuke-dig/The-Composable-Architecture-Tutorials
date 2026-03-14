d58d7fb2904331f31f08fcb8cac41d5b69fcb710
YusukeYokota <yusuke_yokota_ab@mail.toyota.co.jp>
2026-03-14 23:25:21 +0900

    [section3] アプリへの統合

--- The-Composable-Architecture-Tutorials/The_Composable_Architecture_TutorialsApp.swift
+++ The-Composable-Architecture-Tutorials/The_Composable_Architecture_TutorialsApp.swift
@@ -3,11 +3,14 @@ import SwiftUI
 
 @main
 struct The_Composable_Architecture_TutorialsApp: App {
+    static let store = Store(initialState: CounterFeature.State()) {
+        CounterFeature()
+            ._printChanges()
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView(store: Store(initialState: CounterFeature.State()) {
-                CounterFeature()
-            })
+            ContentView(store: Self.store)
         }
     }
 }
