c64005aa8a87b653b3e15fb5174c1e62c7477cdd
YusukeYokota <yusuke_yokota_ab@mail.toyota.co.jp>
2026-03-15 17:17:09 +0900

    [Testing your feature section1] 状態変化のテスト

---
+++ The-Composable-Architecture-TutorialsTests/The_Composable_Architecture_TutorialsTests.swift
@@ -0,0 +1,16 @@
+//
+//  The_Composable_Architecture_TutorialsTests.swift
+//  The-Composable-Architecture-TutorialsTests
+//
+//  Created by yusukeyokota on 2026/03/15.
+//
+
+import Testing
+
+struct The_Composable_Architecture_TutorialsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
